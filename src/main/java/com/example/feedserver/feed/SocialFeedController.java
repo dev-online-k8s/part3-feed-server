0d3d1a63d0ec8cb61e3b6b533cc29711f883ff69
jheo <papercut@gmail.com>
2024-01-08 04:19:53 +0900

    ch3-3. User Server Integration

--- src/main/java/com/example/feedserver/feed/SocialFeedController.java
+++ src/main/java/com/example/feedserver/feed/SocialFeedController.java
@@ -3,6 +3,7 @@ package com.example.feedserver.feed;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,8 +17,17 @@ public class SocialFeedController {
     }
 
     @GetMapping
-    public List<SocialFeed> getAllFeeds() {
-        return feedService.getAllFeeds();
+    public List<FeedInfo> getAllFeeds() {
+        List<SocialFeed> allFeeds = feedService.getAllFeeds();
+
+        List<FeedInfo> result = new ArrayList<>();
+        for (SocialFeed feed: allFeeds) {
+            UserInfo user = feedService.getUserInfo(feed.getUploaderId());
+            FeedInfo feedInfo = new FeedInfo(feed, user.getUsername());
+            result.add(feedInfo);
+        }
+
+        return result;
     }
 
     @GetMapping("/user/{userId}")
